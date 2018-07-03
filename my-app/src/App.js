f5349e187e842782a41f4269e792234804e7375a
danshook <shook.dan@gmail.com>
2018-07-02 17:03:39 -0700

    adding html input boxes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,20 +5,30 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-    <div className="jumbotron">
+      <div className="wrapper">
+    <div className="jumbotron text-center">
     <h1 className="display-4">NEW YORK TIMES ARTICLE SCRUBBER</h1>
+    <p className="lead">Search for and annotate articles of interest.</p>
+
     <hr className="my-4"/>
-    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
   </div>
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
+<div className="input-group mb-3">
+  <div className="input-group-prepend">
+  </div>
+  <input type="text" placeholder="Topic" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
+</div>
+
+<div className="input-group mb-3">
+  <div className="input-group-prepend">
+  </div>
+  <input type="text" placeholder="Start Year" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
+</div>
+<div className="input-group mb-3">
+  <div className="input-group-prepend">
+  </div>
+  <input type="text" placeholder="End Year"className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
+</div>
+</div>
     );
   }
 }
